cde22cc8eddc8ceaf38e3fac34e33d463da37921
unknown <diegofalconc@hotmail.com>
2021-05-20 14:23:19 -0500

    Adding API

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ import express from 'express';
 import handlebars from 'express-handlebars';
 import path from 'path';
 import { Movie } from "./model/movie.js";
+import cors from 'cors';
 
 const app = express();
 
 const __dirname = path.resolve(path.dirname(''));
 
+app.use('/api', cors());
 app.set('view engine', 'handlebars');
 app.set('port', 3000);
 app.engine('handlebars', handlebars({
@@ -17,12 +19,49 @@ app.use(express.static('public'));
 app.use(express.urlencoded());
 
 
-
-
-app.get('/', (req, res, next) => {
+//API USE
+app.get('/api/movies', (req, res) => {
     Movie.find({}).lean()
         .then((movies) => {
-            console.log(movies);
+            res.json(movies);
+        })
+        .catch(err => next(err));
+    
+});
+
+app.get('/api/movies/detail', (req, res, next) => {
+    Movie.findOne({movie: req.query.movie }).lean()
+    .then((movie) => {
+        res.json(movie);
+    })
+    .catch(err => next(err));
+});
+
+app.get('/api/movies/detail/:movie', (req, res, next) => {
+    Movie.findOne({movie: req.params.movie }).lean()
+    .then((movie) => {
+        res.json(movie);
+    })
+    .catch(err => next(err));
+});
+
+app.post('/api/movies/add', (req, res, next) => {
+    console.log(req.body);
+
+    res.json({"message": "Object has been added correctly"});
+});
+
+app.post('/api/movies/delete', (req, res, next) => {
+    console.log(req.body);
+
+    res.json({"message": "Object has been removed correctly"});
+});
+
+
+
+app.get('/', (req, res) => {
+    Movie.find({}).lean()
+        .then((movies) => {
             res.render('home', { layout: 'index', movies });
         })
         .catch(err => next(err));
